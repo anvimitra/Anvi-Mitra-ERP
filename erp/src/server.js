9288231b586bb0f2e32c4173ef48385cd550a563
anvimitra <151990753+anvimitra@users.noreply.github.com>
2026-09-11 16:31:04 +0530

    Wire Super Admin school management UI

--- erp/src/server.js
+++ erp/src/server.js
@@ -15,7 +15,8 @@ app.use(cors({ origin: process.env.CORS_ORIGIN || true, credentials: true }));
 app.use(express.json({ limit: '1mb' }));
 app.use(express.static(path.join(__dirname, '../web')));
 
-app.get('/', (_req,res) => res.sendFile(path.join(__dirname,'../web/super-admin-schools.html')));
+app.get('/', (_req,res) => res.sendFile(path.join(__dirname,'../web/super_admin_schools.html')));
+app.get('/super-admin/schools', (_req,res) => res.sendFile(path.join(__dirname,'../web/super_admin_schools.html')));
 
 app.get('/api/health', async (_req, res) => {
   let database = 'not-configured';
