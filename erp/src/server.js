3cbaf06db6d60cb93bbaf43404b0faa7800ac92a
anvimitra <151990753+anvimitra@users.noreply.github.com>
2026-09-15 16:55:51 +0530

    Expose attendance web routes

--- erp/src/server.js
+++ erp/src/server.js
@@ -22,6 +22,8 @@ app.get('/super-admin/school', (_req,res) => res.sendFile(path.join(__dirname,'.
 app.get('/super-admin/teacher-permissions', (_req,res) => res.sendFile(path.join(__dirname,'../web/super-admin/teacher-assignments.html')));
 app.get('/super-admin/sync-conflicts', (_req,res) => res.sendFile(path.join(__dirname,'../web/super-admin/sync-conflicts.html')));
 app.get('/school/enrollment', (_req,res) => res.sendFile(path.join(__dirname,'../web/school/enrollment.html')));
+app.get('/attendance', (_req,res) => res.sendFile(path.join(__dirname,'../../attendance.html')));
+app.get('/attendance-report', (_req,res) => res.sendFile(path.join(__dirname,'../../attendance-report.html')));
 
 app.get('/api/health', async (_req, res) => {
   let database = 'not-configured';
