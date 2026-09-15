186c72e4f0aff65f9ecbffe1d54fdbcdfef31bb1
anvimitra <151990753+anvimitra@users.noreply.github.com>
2026-09-15 15:06:23 +0530

    Expose teacher permission management page

--- erp/src/server.js
+++ erp/src/server.js
@@ -20,6 +20,7 @@ app.get('/', (_req,res) => res.sendFile(superAdminPage));
 app.get('/super-admin/schools', (_req,res) => res.sendFile(superAdminPage));
 app.get('/super-admin/school-onboarding', (_req,res) => res.sendFile(path.join(__dirname,'../web/super-admin/school-onboarding.html')));
 app.get('/super-admin/school', (_req,res) => res.sendFile(path.join(__dirname,'../web/super-admin/school.html')));
+app.get('/super-admin/teacher-permissions', (_req,res) => res.sendFile(path.join(__dirname,'../web/super-admin/teacher-assignments.html')));
 
 app.get('/api/health', async (_req, res) => {
   let database = 'not-configured';
