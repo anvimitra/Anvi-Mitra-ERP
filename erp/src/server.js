41a52739e26f41462fe0509750da7ba64873a1e5
anvimitra <151990753+anvimitra@users.noreply.github.com>
2026-09-17 18:34:03 +0530

    Add local storage connector route

--- erp/src/server.js
+++ erp/src/server.js
@@ -21,6 +21,7 @@ app.get('/super-admin/school', (_req,res) => res.sendFile(path.join(__dirname,'.
 app.get('/super-admin/teacher-permissions', (_req,res) => res.sendFile(path.join(__dirname,'../web/super-admin/teacher-assignments.html')));
 app.get('/super-admin/sync-conflicts', (_req,res) => res.sendFile(path.join(__dirname,'../web/super-admin/sync-conflicts.html')));
 app.get('/super-admin/sync-monitoring', (_req,res) => res.sendFile(path.join(__dirname,'../web/super-admin/sync-monitoring.html')));
+app.get('/super-admin/local-storage', (_req,res) => res.sendFile(path.join(__dirname,'../web/super-admin/local-storage.html')));
 app.get('/school/enrollment', (_req,res) => res.sendFile(path.join(__dirname,'../web/school/enrollment.html')));
 app.get('/school/staff', (_req,res) => res.sendFile(path.join(__dirname,'../web/school/staff.html')));
 app.get('/staff-attendance', (_req,res) => res.sendFile(path.join(__dirname,'../web/staff-attendance.html')));
